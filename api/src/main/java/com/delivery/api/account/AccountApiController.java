cb3c96a5f99c062c9d7cdfcede2174bfd956270c
Reddyong <d0209hong@naver.com>
2024-06-12 17:01:03 +0900

    feat : create exception handler

--- api/src/main/java/com/delivery/api/account/AccountApiController.java
+++ api/src/main/java/com/delivery/api/account/AccountApiController.java
@@ -2,7 +2,9 @@ package com.delivery.api.account;
 
 import com.delivery.api.account.model.AccountMeResponse;
 import com.delivery.api.common.api.Api;
+import com.delivery.api.common.error.ErrorCode;
 import com.delivery.api.common.error.UserErrorCode;
+import com.delivery.api.common.exception.ApiException;
 import com.delivery.db.account.AccountRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,13 +21,21 @@ public class AccountApiController {
     private final AccountRepository accountRepository;
 
     @GetMapping(path = "/me")
-    public Api<Object> me() {
+    public Api<AccountMeResponse> me() {
         var response = AccountMeResponse.builder()
                 .name("홍길동")
                 .email("A@gmail.com")
                 .registeredAt(LocalDateTime.now())
                 .build();
 
-        return Api.ERROR(UserErrorCode.USER_NOT_FOUND, "홍길동 이라는 사용자 없음");
+        var str = "Hello";
+
+        try {
+            var age = Integer.parseInt(str);
+        } catch (Exception e) {
+            throw new ApiException(ErrorCode.SERVER_ERROR, e, "사용자 Me 호출 시 에러 발생");
+        }
+
+        return Api.OK(response);
     }
 }
